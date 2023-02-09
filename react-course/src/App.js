1d1f4aecdb6187dc5df263cfac3f822b945df6f7
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-02-09 22:17:15 +0530

    discussed How to add image in component

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Courses from "./Courses";
+import parrotImg from "./assets/images/6-pink-parrot-png-images-download.png";
 
 function App() {
   return (
     <div className="App">
       <h1>My First React App.</h1>
       <Courses />
+      <img src={parrotImg} alt="Parrot" />
+      <img
+        src={require("./assets/images/6-pink-parrot-png-images-download.png")}
+        alt="Parrot"
+      />
     </div>
   );
 }
