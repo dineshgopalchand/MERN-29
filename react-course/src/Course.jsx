a72e472ff3093c79b99fce3defced5fb9d25011d
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-02-12 17:18:04 +0530

    Discussed how to pass data from child to parent (lifiting the state up)

--- react-course/src/Course.jsx
+++ react-course/src/Course.jsx
@@ -4,17 +4,29 @@ import { Button } from "react-bootstrap";
 import { VscEdit } from "react-icons/vsc";
 import { RiDeleteBin5Line } from "react-icons/ri";
 function Course(props) {
-  const { id, name, status } = props.details;
+  const { details, updateCourse, deleteCourse, className } = props;
+  const { id, name, status } = details;
+  const updateCourseDetails = () => {
+    updateCourse({
+      id,
+      name,
+      status:2
+    });
+  };
   return (
-    <div className="d-flex course">
-      <div className="title">{name}</div>
+    <div className={`d-flex course ${className}`}>
+      <div className={`title  ${status === 2 ? "line-through" : ""}`}>
+        {name}
+      </div>
       {/* <button className="btn btn-primary">Edit</button> */}
-      <Button variant="primary">
-        <VscEdit  />
+      <Button variant="primary"  onClick={updateCourseDetails}>
+        <VscEdit />
         Edit
       </Button>
       {/* <button className="btn btn-danger">Delete</button> */}
-      <Button variant="danger">
+      <Button variant="danger" onClick={()=>{
+        deleteCourse(id);
+      }}>
         <RiDeleteBin5Line />
         Delete
       </Button>
--- react-course/src/Courses.jsx
+++ react-course/src/Courses.jsx
@@ -1,39 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import Course from "./Course";
+const coursesList = [
+  {
+    id: 1,
+    name: "React",
+    status: 1,
+  },
+  {
+    id: 2,
+    name: "HTML",
+    status: 2,
+  },
+  {
+    id: 3,
+    name: "CSS",
+    status: 2,
+  },
+  {
+    id: 4,
+    name: "JS",
+    status: 2,
+  },
+  {
+    id: 5,
+    name: "NodeJS",
+    status: 0,
+  },
+];
+
 const Courses = () => {
   const title = "Course List";
-  const courseList = [
-    {
-      id: 1,
-      name: "React",
-      status: 1,
-    },
-    {
-      id: 2,
-      name: "HTML",
-      status: 2,
-    },
-    {
-      id: 3,
-      name: "CSS",
-      status: 2,
-    },
-    {
-      id: 4,
-      name: "JS",
-      status: 2,
-    },
-    {
-      id: 5,
-      name: "NodeJS",
-      status: 0,
-    },
-  ];
+  const [courseList, setCourseList] = useState(coursesList);
+  const [newCourse, setNewCourse] = useState("");
+  const inputChangeHandler = (event) => {
+    const value = event.target.value;
+    setNewCourse(value);
+  };
+  const addCourse = (event) => {
+    event.preventDefault();
+    // console.log(newCourse);
+    // const cList=[...courseList];
+    // cList.push({
+    //   id:Date.now(),
+    //   name:newCourse,
+    //   status:0
+    // })
+    // setCourseList(cList);
+    setCourseList((prev) => {
+      return [
+        {
+          id: Date.now(),
+          name: newCourse,
+          status: 0,
+        },
+        ...prev,
+      ];
+    });
+    setNewCourse("");
+  };
+  const deleteCourse = (id) => {
+    setCourseList((prev) => {
+      return prev.filter((course) => course.id !== id);
+    });
+  };
+  const updateCourse = (courseDetail) => {
+    // setCourseList((prevCourseList) => {
+    //   const latestCourseList = [...prevCourseList];
+    //   const courseIndex = latestCourseList.findIndex(
+    //     (course) => course.id === courseDetail.id
+    //   );
+    //   latestCourseList.splice(courseIndex, 1, courseDetail);
+    //   console.log(latestCourseList);
+    //   return latestCourseList;
+    // });
+    setCourseList((prevCourseList) => {
+      return prevCourseList.map((course) => {
+        if (course.id === courseDetail.id) {
+          return courseDetail;
+        } else {
+          return course;
+        }
+      });
+    });
+  };
   return (
     <>
       <h3>{title}</h3>
-      {courseList.map((item) => {
-        return <Course details={item} key={item.id}></Course>;
+      <form onSubmit={addCourse}>
+        <div className="form-group inline-form-block mx-3">
+          <input
+            type="text"
+            value={newCourse}
+            className="form-control"
+            placeholder="Add new course"
+            name="newcourse"
+            onChange={inputChangeHandler}
+          />
+          <button className="btn btn-primary">Add</button>
+        </div>
+      </form>
+      {courseList.map((item,index) => {
+        return (
+          <Course
+            details={item}
+            updateCourse={updateCourse}
+            deleteCourse={deleteCourse}
+            key={item.id}
+            className={`${index%2===0?'light-color':''}`}
+          ></Course>
+        );
       })}
     </>
   );
