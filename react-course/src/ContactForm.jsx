a188cffc08b4eb0edb12afb2d60e7faa2d67a03c
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-02-23 21:48:06 +0530

    Simple form using useState

--- react-course/src/ContactForm.jsx
+++ react-course/src/ContactForm.jsx
@@ -1,30 +1,71 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const ContactForm = () => {
-    const fullNameRef=useRef();
-    const emailRef=useRef();
-    const subjectRef=useRef();
-    const detailsRef=useRef();
-    const contactFormHandler = (event)=>{
-        event.preventDefault();
-        const fullnameVal=fullNameRef.current.value;
-        const emailVal=emailRef.current.value;
-        const subjectVal=subjectRef.current.value;
-        const detailVal=detailsRef.current.value;
-        if(fullnameVal.length<3){
-            return fullNameRef.current.focus();
-        }
-        if(!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailVal)){
-            return emailRef.current.focus();
-        }
+  const [formVal, setFormval] = useState({
+    fullname: "",
+    email: "",
+    subject: "",
+    detail: "",
+  });
 
-        // fullNameRef.current.focus();
-    }
+  const fullNameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const detailsRef = useRef();
+  const contactFormHandler = (event) => {
+    event.preventDefault();
+    // const fullnameVal = fullNameRef.current.value;
+    // const emailVal = emailRef.current.value;
+    // const subjectVal = subjectRef.current.value;
+    // const detailVal = detailsRef.current.value;
+    // if (fullnameVal.length < 3) {
+    //   return fullNameRef.current.focus();
+    // }
+    // if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailVal)) {
+    //   return emailRef.current.focus();
+    // }
+
+    // fullNameRef.current.focus();
+    console.log(formVal);
+    // After validation call API to save data
+  };
+  const fullnameChangeHandle = (event) => {
+    setFormval((prevVal) => {
+      return {
+        ...prevVal,
+        fullname: event.target.value,
+      };
+    });
+  };
+  const emailChangeHandle = (event) => {
+    setFormval((prevVal) => {
+      return {
+        ...prevVal,
+        email: event.target.value,
+      };
+    });
+  };
+  const detailChangeHandle = (event) => {
+    setFormval((prevVal) => {
+      return {
+        ...prevVal,
+        detail: event.target.value,
+      };
+    });
+  };
+  const subjectChangeHandle = (event) => {
+    setFormval((prevVal) => {
+      return {
+        ...prevVal,
+        subject: event.target.value,
+      };
+    });
+  };
   return (
     <>
       <div className="container">
         <h3>Contact Form</h3>
-        <form  onSubmit={contactFormHandler}>
+        <form onSubmit={contactFormHandler}>
           <div className="form-group">
             <label htmlFor="fullname">Full Name</label>
             <input
@@ -34,6 +75,8 @@ const ContactForm = () => {
               name="fullname"
               placeholder="Full name"
               ref={fullNameRef}
+              value={formVal.fullname}
+              onChange={fullnameChangeHandle}
             />
           </div>
           <div className="form-group">
@@ -45,6 +88,8 @@ const ContactForm = () => {
               name="email"
               placeholder="Email"
               ref={emailRef}
+              value={formVal.email}
+              onChange={emailChangeHandle}
             />
           </div>
           <div className="form-group">
@@ -56,6 +101,8 @@ const ContactForm = () => {
               name="subject"
               placeholder="Subject"
               ref={subjectRef}
+              value={formVal.subject}
+              onChange={subjectChangeHandle}
             />
           </div>
           <div className="form-group">
@@ -66,6 +113,8 @@ const ContactForm = () => {
               name="details"
               placeholder="Add some detail"
               ref={detailsRef}
+              value={formVal.detail}
+              onChange={detailChangeHandle}
             ></textarea>
           </div>
           <div className="form-group mt-2">
