a3c2117e98a3df10e5d4df16f23420e3c73c73e0
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-02-28 21:50:38 +0530

    Data fetching using Axios

--- react-course/src/utils/fetch.js
+++ react-course/src/utils/fetch.js
@@ -1,9 +1,21 @@
-const getData = async (url, option = {}) => {
+import axios from "axios";
+// const getData = async (url, option = {}) => {
+//   return new Promise(async (resolve, reject) => {
+//     const fetchedData = await fetch(url, option);
+//     if (fetchedData.ok) {
+//       resolve(fetchedData.json());
+//     } else {
+//       resolve([]);
+//     }
+//   });
+// };
+const getData = async (url, options = {}) => {
   return new Promise(async (resolve, reject) => {
-    const fetchedData = await fetch(url, option);
-    if (fetchedData.ok) {
-      resolve(fetchedData.json());
-    } else {
+    const requestOption = { ...options, url };
+    try {
+      const fetchedData = await axios(requestOption);
+      resolve(fetchedData.data);
+    } catch (err) {
       resolve([]);
     }
   });
