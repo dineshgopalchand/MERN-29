f6f9a7acdd86c2397bd7531382550fc79fa3679d
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-03-04 21:05:09 +0530

    introduced SCSS in react

--- react-course-ts/src/App.tsx
+++ react-course-ts/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-     <h2>React with TS</h2>
+      <h2>React with TS</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium,
+        veritatis.
+      </p>
+      <div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium,
+          veritatis.
+        </p>
+      </div>
     </div>
   );
 }
