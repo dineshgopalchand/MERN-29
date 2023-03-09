c17639ceb39c007a29b0c7dcaec5f2ed22abdd68
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2023-03-09 22:27:43 +0530

    Sample Login functionality added

--- react-course-ts/src/UI/Header.tsx
+++ react-course-ts/src/UI/Header.tsx
@@ -1,6 +1,9 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import classes from "./Header.module.scss";
+import { FaUserCircle } from "react-icons/fa";
+import { useReducer, useState } from "react";
 const navList: INavLink[] = [
   {
     id: 1,
@@ -19,18 +22,69 @@ const navList: INavLink[] = [
   },
 ];
 
+const loginInitVal: ILoginVal = {
+  isLogin: false,
+};
+const loginReducer = (state: ILoginVal, action: ILoginAction): ILoginVal => {
+  switch (action.type) {
+    case "login":
+      return { isLogin: true, details: action.payload };
+    case "logout":
+      return { isLogin: false };
+    default:
+      return state;
+  }
+};
 function Header() {
+  const [loginDetail, loginDispatch] = useReducer(loginReducer, loginInitVal);
+  const [loginPending,setLoginPending]=useState(false);
   const navElement = navList.map((nav) => (
     <Nav.Link href={nav.link} key={nav.id}>
       {nav.title}
     </Nav.Link>
   ));
+  const loginHandler = async () => {
+    setLoginPending(true);
+    const userDetails: ILoginDetails = await new Promise((resolve) => {
+      setTimeout(() => {
+        const details: ILoginDetails = {
+          email: "dinesh@gmail.com",
+          name: "Dinesh",
+        };
+        resolve(details);
+      }, 2000);
+    });
+    loginDispatch({
+      type: "login",
+      payload: userDetails,
+    });
+    setLoginPending(false);
+  };
+  const logoutHandler = () => {
+    loginDispatch({
+      type: "logout",
+    });
+  };
   return (
     <>
       <Navbar bg="primary" variant="dark">
         <Container fluid>
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">{navElement}</Nav>
+          <div>
+            {loginDetail.isLogin ? (
+              <>
+                <button className="btn btn-danger" onClick={logoutHandler}>
+                  Logout
+                </button>
+                <FaUserCircle className={`${classes.userIcon} m-1`} />
+              </>
+            ) : (
+              <button className="btn btn-warning" onClick={loginHandler} disabled={loginPending}>
+                Login
+              </button>
+            )}
+          </div>
         </Container>
       </Navbar>
     </>
@@ -43,4 +97,16 @@ interface INavLink {
   id: number;
   title: string;
   link: string;
+}
+export interface ILoginVal {
+  isLogin: boolean;
+  details?: ILoginDetails;
+}
+export interface ILoginDetails {
+  name: string;
+  email: string;
+}
+export interface ILoginAction {
+  type: "login" | "logout";
+  payload?: ILoginDetails;
 }
